303601896a9148ad5bb068d52c5b2595e3b34659
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-06-09 01:59:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+<<<<<<< HEAD
 const divide = () => 2000/100
 const square = x => x * x;
 const add = (x, y) => x + y;
+=======
+function foo() {
+  return 'bar';
+}
+
+const foo = () => {
+  return 'bar';
+}
+>>>>>>> 14179aa11d92064664aed7def5b4d20a0cb11a52
